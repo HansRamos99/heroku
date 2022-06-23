0a485c9a7fd976d64e859ba46989f7448cc62bad
unknown <hempghans@gmail.com>
2022-06-23 18:49:42 -0500

    primer comentario

---
+++ scripts/ejercicio.js
@@ -0,0 +1,44 @@
+/*var ejecutarCodigo=()=>{
+    alert('Hola mundo');
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    ejecutarCodigo();
+});
+*/
+
+var titulo1= document.getElementById("titulo1");
+titulo1.innerText= "Album de fotos";
+
+
+var text_muted= document.getElementsByClassName("text-muted");
+text_muted[1].innerHTML="<span> En este sitio encontrarás un album de fotos inspirado en el snippet de <a href='https://codepen.io/taj1uddin/pen/eYVrLKy'>Codepen - Taj Uddin</a>.</span>";
+//text_muted.innerHTML= "<span> En este sitio encontrarás un album de fotos inspirado en el snippet de <a href='https://codepen.io/taj1uddin/pen/eYVrLKy'>Codepen - Taj Uddin</a>.</span>"
+
+var parrafos= document.getElementsByTagName("p");
+parrafos[2].className= 'd-none'
+
+var arreglo=[{url: 'https://images.unsplash.com/photo-1653942786759-f3caff948222?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'camino'},
+{url:'https://images.unsplash.com/photo-1653988235129-842891001e10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt:'energia'},
+ { url: 'https://images.unsplash.com/photo-1648737963540-306235c8170e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'papa'},
+{url: "https://images.unsplash.com/photo-1648737963540-306235c8170e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt:'' },
+{url: "https://images.unsplash.com/photo-1653936999234-6574484abc38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMTd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt:'' },
+{url: "https://images.unsplash.com/photo-1653559260129-d4347e4c8df0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMjJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt: '' }, 
+{url: "https://images.unsplash.com/photo-1648737962619-16e5cd465d2b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyOTF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt: '' }, 
+{url: "https://images.unsplash.com/photo-1578255321055-d9ed6a976af5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt:''}, 
+{url: "https://images.unsplash.com/photo-1654002300319-6c882ad161ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt:'' } ]
+
+let elementos= document.getElementsByTagName('svg');
+
+
+
+for(let i=0; i<9;i++){
+    
+
+    elementos[1].outerHTML='<img src="'+arreglo[i].url+'" alt="'+arreglo[i].alt+'"  width=100% height="225"></img>';
+}
+
+
+
+
+
